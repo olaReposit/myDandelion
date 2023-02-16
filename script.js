e2d9f3054e12b1e8382fa5505e3b10b6968e1647
O L A <114856145+olaReposit@users.noreply.github.com>
2023-02-16 14:43:14 +0100

    Update script.js
    more functionality

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const say = document.getElementById('say');
 function showMessage() {
     say.innerHTML="I LOVE YOU, NAOMI!";
 }
+
+const song = document.getElementById("song");
+
+function playSong(){
+    // alert('playing');
+    song.play();
+}
